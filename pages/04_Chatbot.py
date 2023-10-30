e6b2a9ba056cc733bcc0b2db12d760ab9de37379
Hoon-Jung <hoonjung06@gmail.com>
2023-10-29 20:41:28 -0400

    fully functional chatbot AI

--- pages/04_Chatbot.py
+++ pages/04_Chatbot.py
@@ -2,40 +2,50 @@ import streamlit as st
 
 import openai
 from langchain.chat_models import ChatOpenAI
-from langchain import LLMChain
+from langchain.chains import LLMChain
 import utils
 
-import os
-from langchain.prompts import PromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
 )
 
 
-def config_openai(p):
-    ai = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.session_state.openai_api_key)
+class textStreamingHandler(BaseCallbackHandler):
+    def __init__(self, stcontainer):
+        self.tb = stcontainer
+        self.text = ""
+    def on_llm_new_token(self, token,**kwargs):
+        self.text += token
+        self.tb.write(self.text)
+
+
+@st.cache_resource
+def config_openai():
+    ai = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.session_state.openai_api_key, streaming=True)
 
     template = "A Friendly assistant"
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
     human_template = "You will respond to this chat in a friendly manner: {text}"
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
     
-    # chainai = LLMChain(llm=ai, prompt=chat_prompt)
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, MessagesPlaceholder(variable_name="chat_history"),human_message_prompt])
+    
+    memory = ConversationBufferWindowMemory(k=5, memory_key="chat_history", return_messages=True)
+    chainai = LLMChain(llm = ai, prompt=chat_prompt, memory=memory)
 
-    # prompt_template = "Respectfully answer: {chat}?"
-    chainai = LLMChain(llm = ai, prompt=chat_prompt)
-    result = chainai(p)
-
-    return result["text"]
+    return chainai
 
 
 
 if __name__ == "__main__":
     utils.set_openai_api_key()
     st.title("Simple AI Chatbot")
+    ai = config_openai()
     
     if "msg" not in st.session_state:
         st.session_state.msg = []
@@ -52,7 +62,7 @@ if __name__ == "__main__":
             st.session_state.msg.append({"role": "user", "content": prompt})
 
         with st.chat_message("assistant"):
-             answer = config_openai(prompt)
-             st.write(answer)
+             textbox = textStreamingHandler(st.empty())
+             answer = ai.run(text=prompt, callbacks=[textbox])
              st.session_state.msg.append({"role": "assistant", "content": answer})
 

374c1c4b16de606f7a933ce8f3462682852a81bb
Hoon-Jung <hoonjung06@gmail.com>
2023-11-05 21:54:11 -0500

    Finishing changes to document search

--- pages/05_Document_Search.py
+++ pages/05_Document_Search.py
@@ -1,9 +1,12 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from PyPDF2 import PdfReader
+from streamlit_js_eval import streamlit_js_eval
 
 import os
 import streamlit as st
@@ -11,13 +14,40 @@ from io import StringIO
 import utils
 
 
+def clear():
+    st.session_state["text"] = ""
+
 
 def file_selector():
-        folder_path = os.path.join(".", "data", "pdfs")
+        folder_path = os.path.join(".", "data", "db", "document_search_folder")
         filenames = os.listdir(folder_path)
-        selected_filename = st.selectbox('Select a file', filenames)
+        filenames.append("Upload a file")
+        selected_filename = st.selectbox('Select a file', filenames, on_change=clear)
         
         return os.path.join(folder_path, selected_filename), selected_filename
+
+
+
+def file_upload():
+    uploadedfile = st.file_uploader("Choose a file", "pdf")
+    if uploadedfile is not None:
+        texts = ""
+        
+        file = PdfReader(uploadedfile)
+        with st.spinner("Loading pdf..."):
+            for page in file.pages:
+                texts += page.extract_text()
+            
+            splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            doc = splitter.create_documents([texts])
+
+            selected_db = FAISS.from_documents(doc, HuggingFaceEmbeddings())
+            selected_db.save_local(os.path.join(".", "data", "db", "document_search_folder", uploadedfile.name))
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+        return uploadedfile.name
+
+
+#when i upload a file make the multiselect automatically switch to the new file
 
 
 
@@ -28,28 +58,18 @@ if __name__ == "__main__":
     st.title("AI Document Search")
 
     filename, selected_doc = file_selector()
-    st.write('You selected `%s`' % filename)
 
-    if filename is not None:
-        abs_path= os.path.abspath(filename)
-
-        try:
+    if filename:
+        if selected_doc == "Upload a file":
+             selected_doc = file_upload()
+        else:
+             st.write('You selected `%s`' % filename)
+             abs_path= os.path.abspath(filename)
              selected_db = FAISS.load_local(os.path.join(".", "data", "db", "document_search_folder", selected_doc), HuggingFaceEmbeddings())
-        except:
-             print("Error with finding DB")
-             with st.spinner("Making DB..."):
-                raw_documents = PyPDFLoader(abs_path).load_and_split()
-                
-                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-                documents = text_splitter.split_documents(raw_documents)
-
-                selected_db = FAISS.from_documents(documents, HuggingFaceEmbeddings())
-                selected_db.save_local(os.path.join(".", "data", "db", "document_search_folder", selected_doc))
-
-        question = st.text_input("Enter your question about this file.")
-        if question:
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=st.session_state.openai_api_key)
-            qa_chain = RetrievalQA.from_chain_type(llm, retriever=selected_db.as_retriever())
-            with st.spinner("Searching..."):
-                res = qa_chain({"query": question})
-            st.write(res["result"])
+             question = st.text_input("Enter your question about this file.", key="text")
+             if question:
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=st.session_state.openai_api_key)
+                qa_chain = RetrievalQA.from_chain_type(llm, retriever=selected_db.as_retriever())
+                with st.spinner("Searching..."):
+                    res = qa_chain({"query": question})
+                st.write(res["result"])
